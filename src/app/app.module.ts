1a8c413fcae9e5bf1c529f7caec3855b205e5dde
Marcos Paulo Belasco de Almeida <patu.duck@gmail.com>
2018-03-09 23:29:30 -0300

    Showing import tasks on dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { DashboardComponent } from "./dashboard/dashboard.component";
 import { NavbarComponent } from './navbar/navbar.component';
 import { TasksComponent } from "./tasks/tasks.component";
 import { TaskDetailComponent } from "./tasks/task-detail/task-detail.component";
+import { TaskService } from "./tasks/shared/task.service";
 
 const ROUTES = RouterModule.forRoot([
   {
@@ -38,7 +39,7 @@ const ROUTES = RouterModule.forRoot([
     FormsModule,
     ROUTES
   ],
-  providers: [],
+  providers: [TaskService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
