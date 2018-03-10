905a273cbd8b1629b6afa5231431295d6abbf62c
Marcos Paulo Belasco de Almeida <patu.duck@gmail.com>
2018-03-10 19:35:48 -0300

    Adding AppRountingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { NavbarComponent } from './navbar/navbar.component';
 import { TasksComponent } from "./tasks/tasks.component";
 import { TaskDetailComponent } from "./tasks/task-detail/task-detail.component";
+
 import { TaskService } from "./tasks/shared/task.service";
 
-const ROUTES = RouterModule.forRoot([
-  {
-    path: 'tasks/:id',
-    component: TaskDetailComponent
-  },
-  {
-    path: 'dashboard',
-    component: DashboardComponent
-  },
-  {
-    path: 'tasks',
-    component: TasksComponent
-  },
-  {
-    path: '',
-    redirectTo: '/dashboard',
-    pathMatch: 'full'
-  }
-])
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -41,7 +23,7 @@ const ROUTES = RouterModule.forRoot([
   imports: [
     BrowserModule,
     FormsModule,
-    ROUTES
+    AppRoutingModule
   ],
   providers: [TaskService],
   bootstrap: [AppComponent]
